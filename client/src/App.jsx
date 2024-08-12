82efba26758d8876c820c422d7bee95d53d6c62b
Gungun-Pathak <gungunpathak600@gmail.com>
2024-08-12 17:41:46 +0530

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div className="text-4xl">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AiOulineSearch } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { Navbar, TextInput } from "flowbite-react";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
