560b9e1b37aab519d41cf92be281ab5397e856a4
Gungun-Pathak <gungunpathak600@gmail.com>
2024-08-16 13:09:03 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ app.listen(3000,()=>{
   console.log('server is running')
 
 });
+app.use(express.json());
 
 app.use('/api/user',userRoutes );
+app.use('/api/auth',authRoutes);
 
