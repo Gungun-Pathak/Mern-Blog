4f9f607c65f0c0df7d3bcd1d45ab814669c38304
Gungun-Pathak <gungunpathak600@gmail.com>
2024-08-17 15:45:47 +0530

    sign up ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
 
 
-export const signup=async(req,res)=>{
+export const signup=async(req,res,next)=>{
   const {username,email,password}=req.body;
   if(!username || !password || !email || username==="" || password===""  || email===""){
-    return res.status(400).json({message:"All Fields Required"});
+    next(errorHandler(400,"All fields are required"));
   }
   const hashedPassword=bcryptjs.hashSync(password,10);
   const newUser=new User({
@@ -21,7 +22,7 @@ export const signup=async(req,res)=>{
     res.json("Signup Successful");
 
   }catch(error){
-    res.status(500).json({message:error.message});
+    next(error);
 
   }
  
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Label, TextInput, Button } from "flowbite-react";
+import { Link } from "react-router-dom";
 
-const Signup = () => {
+export default function Signup() {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="min-h-screen  flex items-center justify-center">
+      <div className="w-full max-w-md">
+        <form>
+          <div className="mb-4">
+            <Label htmlFor="username" value="Your Username" />
+            <TextInput
+              type="text"
+              placeholder="Username"
+              id="username"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <Label htmlFor="email" value="Your Email" />
+            <TextInput type="email" placeholder="Email" id="email" required />
+          </div>
+          <div>
+            <Label htmlFor="password" value="Your Password" />
+            <TextInput
+              type="password"
+              placeholder="Password"
+              id="password"
+              required
+            />
+          </div>
 
-export default Signup
+          <div className="mb-4 mt-4 w-full">
+            <Button
+              className="w-full"
+              gradientDuoTone="purpleToPink"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </div>
+        </form>
+        <div className="flex gap-2">
+          <span>Have an account?</span>
+          <Link to="/sign-in" className="text-blue-500">
+            {" "}
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
