9a9846a16287d802c21a90b785435f7a5c90c6d6
Gungun-Pathak <gungunpathak600@gmail.com>
2024-08-22 13:00:12 +0530

    functionality in signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,24 +1,62 @@
 import React from "react";
-import { Label, TextInput, Button } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Label, TextInput, Button, Alert } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields");
+    }
+    try {
+      const res = await fetch("api/auth/signup", {
+        method: "Post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      if (res.ok) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen  flex items-center justify-center">
       <div className="w-full max-w-md">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <Label htmlFor="username" value="Your Username" />
             <TextInput
               type="text"
               placeholder="Username"
               id="username"
+              onChange={handleChange}
               required
             />
           </div>
           <div className="mb-4">
             <Label htmlFor="email" value="Your Email" />
-            <TextInput type="email" placeholder="Email" id="email" required />
+            <TextInput
+              type="email"
+              placeholder="Email"
+              id="email"
+              onChange={handleChange}
+              required
+            />
           </div>
           <div>
             <Label htmlFor="password" value="Your Password" />
@@ -26,6 +64,7 @@ export default function Signup() {
               type="password"
               placeholder="Password"
               id="password"
+              onChange={handleChange}
               required
             />
           </div>
@@ -47,6 +86,11 @@ export default function Signup() {
             Sign in
           </Link>
         </div>
+        {errorMessage && (
+          <Alert className="mt-6" color="failure">
+            {errorMessage}
+          </Alert>
+        )}
       </div>
     </div>
   );
